ca5caf56c1c11d28e06fb1cde7220ad034f8e125
Emily Sousa <diassousa@yahoo.com.br>
2021-06-16 11:26:18 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import Begin from './pages/begin';
+import './App.css';
+
+function App() {
+  return (
+    <Begin/>
+  );
+}
+
+export default App;
---
+++ src/pages/begin.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Begin = () => {
+  return(
+    <div>
+      They call themself the gardians of the galaxy
+    </div>
+  )
+}
+
+export default Begin;
